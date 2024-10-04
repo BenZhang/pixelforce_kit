bf4a11572ddfb0064a7ee4a46316f4a7392aad53
Ben Zhang <benzhang@Bens-iMac.modem>
2024-10-04 23:27:39 +0930

    final tweak

--- lib/pixelforce_kit/spec_helper.rb
+++ lib/pixelforce_kit/spec_helper.rb
@@ -44,6 +44,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
+  config.render_views
 
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
