3aba3cb423c73e81e720efd573e7cad2dd7e54bd
BenZhang <ben.zhang@pixelforce.com.au>
2024-10-04 11:33:03 +0930

    tweak spec_helper

--- lib/pixelforce_kit/spec_helper.rb
+++ lib/pixelforce_kit/spec_helper.rb
@@ -25,6 +25,7 @@ module AuthenticationHelpers
 end
 
 RSpec.configure do |config|
+  config.exclude_pattern = 'spec/integration/**/*_spec.rb'
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
