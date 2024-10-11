6c49e5192819f2bdeee4a3e722e0f1ffa8e27119
Justin Li <justin.li@pixelforce.com.au@Justins-MacBook-Pro.local>
2024-10-11 17:48:26 +1030

    update recipes to kit

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in pixelforce_recipes.gemspec
+# Specify your gem's dependencies in pixelforce_kit.gemspec
 gemspec
--- lib/pixelforce_kit/recipes.rb
+++ lib/pixelforce_kit/recipes.rb
@@ -1,4 +1,4 @@
-require "pixelforce_recipes/version"
+require "pixelforce_kit/version"
 require "capistrano"
 
 Dir[File.expand_path("#{File.dirname(__FILE__)}/recipes/capistrano_recipes/*.rb")].each {|file| require file }
