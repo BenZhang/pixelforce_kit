4d072dca74f1fb94f8a77aabe9f27f668d6d0624
Justin Li <justin.li@pixelforce.com.au@Justins-MacBook-Pro.local>
2024-10-16 12:19:36 +1030

    update factory

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :admin_user do
+    first_name { 'John' }
+    last_name { 'Doe' }
+    sequence(:email) { |n| "username+#{n}@example.com" }
+    password { 'Password123!' }
+    provider { 'email' }
+    allow_password_change { false }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :user do
+    first_name { 'John' }
+    last_name { 'Doe' }
+    sequence(:email) { |n| "username+#{n}@example.com" }
+    password { 'Password123!' }
+    provider { 'email' }
+    allow_password_change { false }
+    global_notification { true }
+    turned_off_notification_category_ids { [] }
   end
 end
